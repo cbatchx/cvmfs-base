fcad42b460ab9097ecd2b91d0abc1d6c828b56cd
Didrik <didrik.rognstad@gmail.com>
2016-04-27 21:17:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,21 +15,8 @@ cat /etc/cvmfs/default.local
 # Setup cvmfs
 cvmfs_config setup
 
-# cat > /etc/fstab <<EOF
-# atlas.cern.ch       /cvmfs/atlas.cern.ch cvmfs allow_other 0 0
-# atlas-condb.cern.ch /cvmfs/atlas-condb.cern.ch cvmfs allow_other 0 0
-# grid.cern.ch        /cvmfs/grid.cern.ch cvmfs allow_other 0 0
-# cernvm-prod.cern.ch /cvmfs/cernvm-prod.cern.ch cvmfs allow_other 0 0
-# EOF
-
-
-
-
-
-
 # Test
 cvmfs_config probe
 
 # Run inotifywait to track access
-# inotifywait -m /cvmfs/*
-$*
+inotifywait -m /cvmfs/*
