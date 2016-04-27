089761f6e8a2856c0bc61385fe2acd9206c91161
Didrik A. Rognstad <didrik.rognstad@gmail.com>
2016-04-27 22:34:46 +0200

    more robust

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
+set -e
 
-# Stop autofs
-service autofs stop
+: ${CVMFS_REPOSITORIES:=cernvm-prod.cern.ch}
+: ${CVMFS_HTTP_PROXY:="DIRECT"}
+
 
 # Make config
 cat > /etc/cvmfs/default.local <<EOF
-CVMFS_REPOSITORIES=${CVMFS_REPOSITORIES:-cernvm-prod.cern.ch}
-CVMFS_HTTP_PROXY=${CVMFS_HTTP_PROXY:-"DIRECT"}
+CVMFS_REPOSITORIES=$CVMFS_REPOSITORIES
+CVMFS_HTTP_PROXY=$CVMFS_HTTP_PROXY
 EOF
 
 echo "/etc/cvmfs/default.local is now: "
 cat /etc/cvmfs/default.local
 
+# Unmount old stuff
+cvmfs_config umount
+
 # Setup cvmfs
 cvmfs_config setup
+# Stop autofs
+service autofs stop
+
+# Mounting
+IFS=","
+for r in $CVMFS_REPOSITORIES
+do
+    echo "Mounting $r"
+    mkdir -p /cvmfs/$r
+    mount -t cvmfs $r /cvmfs/$r
+done
 
 # Test
 cvmfs_config probe
 
-# Run inotifywait to track access
+# Run inotifywait to track access and to do something :)
 inotifywait -m /cvmfs/*
