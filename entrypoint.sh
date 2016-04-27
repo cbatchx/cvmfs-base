f142e800355345370a0dbbb6b7ef3d1c0504f071
Didrik A. Rognstad <didrik.rognstad@gmail.com>
2016-04-27 20:18:06 +0200

    dynamic setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,21 +5,24 @@ service autofs stop
 
 # Make config
 cat > /etc/cvmfs/default.local <<EOF
-CVMFS_REPOSITORIES=atlas.cern.ch,atlas-condb.cern.ch,grid.cern.ch,cernvm-prod.cern.ch
-CVMFS_HTTP_PROXY="DIRECT"
+CVMFS_REPOSITORIES=${CVMFS_REPOSITORIES:-cernvm-prod.cern.ch}
+CVMFS_HTTP_PROXY=${CVMFS_HTTP_PROXY:-"DIRECT"}
 EOF
 
-cat > /etc/fstab <<EOF
-atlas.cern.ch       /cvmfs/atlas.cern.ch cvmfs allow_other 0 0
-atlas-condb.cern.ch /cvmfs/atlas-condb.cern.ch cvmfs allow_other 0 0
-grid.cern.ch        /cvmfs/grid.cern.ch cvmfs allow_other 0 0
-cernvm-prod.cern.ch /cvmfs/cernvm-prod.cern.ch cvmfs allow_other 0 0
-EOF
-
-
+echo "/etc/cvmfs/default.local is now: "
+cat /etc/cvmfs/default.local
 
 # Setup cvmfs
 cvmfs_config setup
+
+# cat > /etc/fstab <<EOF
+# atlas.cern.ch       /cvmfs/atlas.cern.ch cvmfs allow_other 0 0
+# atlas-condb.cern.ch /cvmfs/atlas-condb.cern.ch cvmfs allow_other 0 0
+# grid.cern.ch        /cvmfs/grid.cern.ch cvmfs allow_other 0 0
+# cernvm-prod.cern.ch /cvmfs/cernvm-prod.cern.ch cvmfs allow_other 0 0
+# EOF
+
+
 
 
 
@@ -28,4 +31,5 @@ cvmfs_config setup
 cvmfs_config probe
 
 # Run inotifywait to track access
-inotifywait -m /cvmfs/*
+# inotifywait -m /cvmfs/*
+$*
